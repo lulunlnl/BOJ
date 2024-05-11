ddaec63c9a402af4b736ef3fd356681447b4886f
lulunlnl <mangolobstar1@gmail.com>
2024-05-11 20:37:50 +0900

    교차하지 않는 원의 현들의 최대집합

--- 2673/main.cpp
+++ 2673/main.cpp
@@ -1,22 +1,20 @@
-#include "bits/stdc++.h"
+#include <bits/stdc++.h>
 using namespace std;
 int chk[105][105], dp[105][105];
 
 int main() {
-    cin.tie(0)->sync_with_stdio(0);
     int N; cin >> N;
     for (int i = 0; i < N; i++) {
         int a, b; cin >> a >> b;
-        a--; b--;
         chk[a][b] = 1;
         chk[b][a] = 1;
     }
-    for (int d = 1; d <= 99; d++) {
-        for (int i = 0; i < 100; i++) {
-            for (int k = i; k <= i+d; k++) {
-                dp[i][i+d] = max(dp[i][i+d], dp[i][k] + dp[k][i+d] + chk[i][i+d]);
+    for (int i = 1; i <= 100; i++) {
+        for (int j = i; j >= 1; j--) {
+            for (int k = j; k < i; k++) {
+                dp[j][i] = max(dp[j][i], dp[j][k] + dp[k][i] + chk[i][j]);
             }
         }
     }
-    cout << dp[0][99];
+    cout << dp[1][100];
 }
--- Easy/main.cpp
+++ Easy/main.cpp
@@ -1,57 +1,13 @@
 #include <bits/stdc++.h>
 using namespace std;
-int a[15][15], b[15][15];
 
 int main() {
-    int n; cin >> n;
+    int n, m; cin >> n >> m;
+    int cnt = 0, now = 0;
     for (int i = 0; i < n; i++) {
-        for (int j = 0; j <= i; j++) {
-            cin >> a[i][j];
-        }
+        int x; cin >> x;
+        now = max(0, now + x);
+        cnt += now >= m;
     }
-    int ans = 0;
-    for (int i = 0; i < n; i++) {
-        for (int j = 0; j <= i; j++) {
-            cin >> b[i][j];
-            ans += a[i][j] ^ b[i][j];
-        }
-    }
-    int tmp = 0;
-    for (int i = 0; i < n; i++) {
-        for (int j = 0; j <= i; j++) {
-            tmp += a[i][j] ^ b[n - 1 - j + i][i - j];
-        }
-    }
-    ans = min(ans, tmp);
-    tmp = 0;
-
-    for (int i = 0; i < n; i++) {
-        for (int j = 0; j <= i; j++) {
-            tmp += a[i][j] ^ b[n - j - 1][i];
-        }
-    }
-    ans = min(ans, tmp);
-
-    for (int i = 0; i < n; i++) {
-        for (int j = 0; j <= i; j++) {
-            a[i][j] = a[i][i - j];
-        }
-    }
-    tmp = 0;
-
-    for (int i = 0; i < n; i++) {
-        for (int j = 0; j <= i; j++) {
-            tmp += a[i][j] ^ b[n - 1 - j + i][i - j];
-        }
-    }
-    ans = min(ans, tmp);
-    tmp = 0;
-
-    for (int i = 0; i < n; i++) {
-        for (int j = 0; j <= i; j++) {
-            tmp += a[i][j] ^ b[n - j - 1][i];
-        }
-    }
-    ans = min(ans, tmp);
-    cout << ans;
+    cout << cnt;
 }
